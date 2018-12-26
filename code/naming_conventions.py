934ae7f71c82ef397403da5df5fffa65840317a4
Jan Groth <jan.groth.de@gmail.com>
2018-12-27 08:18:07 +1100

    talk

---
+++ code/naming_conventions.py
@@ -0,0 +1,34 @@
+CONSTANTS_IN_UPPERCASE = 123
+
+variables_are_lowercase = 'OH YES!'
+
+
+def methods_are_lowercase_too():
+    pass
+
+
+def all_use_kebap_case():
+    pass
+
+
+# uses supplied credentials to create new sftp client
+def sftp_client(username, password):
+    pass
+
+
+def sftp_client(username, password):
+    """
+    This method uses the supplied credentials to create a new sftp client.
+    """
+    pass
+
+
+def create_sftp_client(username, password):
+    """
+    This method uses the supplied credentials to create a new sftp client.
+    """
+    pass
+
+
+def create_sftp_client(username, password):
+    pass

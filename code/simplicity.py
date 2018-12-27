55bb15dbf282370f1c4b3e5d6289b1e19fd22431
Jan Groth <jan.groth.de@gmail.com>
2018-12-27 16:33:20 +1100

    talk

--- code/simplicity.py
+++ code/simplicity.py
@@ -6,21 +6,26 @@ while counter < 5:
 for i in range(5):
     print(i)
 
+symbols = '$¢£¥€¤'
+codes = []
+for symbol in symbols:
+    codes.append(ord(symbol))
+
+symbols = '$¢£¥€¤'
+codes = [ord(symbol) for symbol in symbols]
+
 
 def pfPasswordHash(arg):
     # Randomise Salt
     salt = os.urandom(6)
     # Convert String to Byte Array
     res = arg.encode()
-    # Hash through 10000 times. This relates to PingFederate Algorithm 5 reference
-    for lp in range(1000001):
-        # Always setup hashlib. This is due to
-        # the digest not resetting after .digest is called
+    # Hash through 10000 times.
+    for lp in range(10000):
+        # Always initialize hashlib, because [...]
         m = hashlib.sha256()
         # Swap between the two inputs into the digest based on loop number
         m.update(res) if lp % 2 else m.update(salt)
         m.update(salt) if lp % 2 else m.update(res)
         # Finish the digest
         res = m.digest()
-    # Return the hashed result and salt in the way PingFederate is expecting
-    return res.encode('base64').rstrip() + "." + salt.encode('base64').rstrip() + ".5"

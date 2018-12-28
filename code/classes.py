354ff8a3eebb4c2fecab92b55eac006f4b7d153b
Jan Groth <jan.groth.de@gmail.com>
2018-12-29 09:15:02 +1100

    talk

--- code/classes.py
+++ code/classes.py
@@ -33,3 +33,23 @@ base_ten_calc = Calculator(base=10)
 print(base_ten_calc.multiply_by(5))
 print(base_ten_calc.add_to(7))
 
+
+class Sftp2S3:
+    def __init__(self, server_name, bucket_name):
+        self.bucket = boto3.resource('s3').Bucket(bucket_name)
+        self.sftp_server = self._init_sftp(server_name)
+
+    def _init_sftp(self, server_name): pass
+
+    def _needs_sync(self, file_name): pass
+
+    def _copy_from_sftp(self, file_name): pass
+
+    def sync_file(self, file_name):
+        if (self._needs_sync(file_name)):
+            self._copy_from_sftp(file_name)
+
+
+s2s_sync = Sftp2S3(server_name='a.b.c', bucket_name='my-bucket')
+s2s_sync.sync_file('foo.txt')
+s2s_sync.sync_file('baz/bar.txt')
